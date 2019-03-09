1e4d0bdc49eb04e2059671893167cd00ddc5e1fb
DobrozhelovaN <36194709+DobrozhelovaN@users.noreply.github.com>
2019-03-09 16:35:45 +0200

    Готово!

--- game.js
+++ game.js
@@ -65,4 +65,16 @@ function draw()
 	          score++;
 	      }
     }
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+
+    ctx.fillStyle = "#000";
+    ctx.font = "24px Verdana";
+    ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+
+    requestAnimationFrame(draw);
 }
+
+pipeBottom.onload = draw;
